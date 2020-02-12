a0defa3935401761fe9e6ead7d130d63fee6dc02
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2020-02-12 14:57:15 +0700

    change manifest

--- src/modules.name/submod.hello/submod.module.ts
+++ src/modules.name/submod.hello/submod.module.ts
@@ -2,13 +2,20 @@
  * @author Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
  */
 
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { SubModController } from './submod.controller';
 import { SubModService } from './submod.service';
+import { HelloLoggerMiddleware } from './middleware/submod.middleware.hello';
 
 @Module({
   controllers: [SubModController],
   providers: [SubModService],
 })
 
-export class SubModModule {}
+export class SubModModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HelloLoggerMiddleware)
+      .forRoutes('submod.hello');
+  }
+}
